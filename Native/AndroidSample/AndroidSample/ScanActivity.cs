20988cda05d21245ee9f4bd04527ef646a3eb024
Silvio Frischknecht <silvio@scandit.com>
2016-09-27 07:37:35 +0200

    check check/get camera permission at runtime on Android 6

--- Native/AndroidSample/AndroidSample/ScanActivity.cs
+++ Native/AndroidSample/AndroidSample/ScanActivity.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Android;
 using Android.App;
 using Android.Content;
+using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
@@ -20,6 +22,10 @@ namespace XamarinScanditSDKSampleAndroid
 	{
 		private BarcodePicker picker;
 		public static string appKey = "--- ENTER YOUR SCANDIT APP KEY HERE ---";
+		private const int CameraRequestPermission = 0; // this int will be returned when we are granted permission
+		private bool mDeniedCameraAccess = false;
+		private bool mPaused = true;
+
 
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -103,13 +109,73 @@ namespace XamarinScanditSDKSampleAndroid
 			}
 		}
 
+		private void grantCameraPermissionsThenStartScanning()
+		{
+			Console.WriteLine("get permission");
+			if (CheckSelfPermission(Manifest.Permission.Camera)
+				!= Permission.Granted)
+			{
+				Console.WriteLine("get permission1");
+
+				if (mDeniedCameraAccess == false)
+				{
+					Console.WriteLine("get permission2");
+
+					// it's pretty clear for why the camera is required. We don't need to give a
+					// detailed reason.
+					RequestPermissions(new String[] { Manifest.Permission.Camera },
+									   CameraRequestPermission);
+				}
+
+			}
+			else {
+				Console.WriteLine("get permission3 " + CheckSelfPermission(Manifest.Permission.Camera));
+
+				// we already have the permission
+				picker.StartScanning();
+			}
+		}
+
+		override public void OnRequestPermissionsResult(int requestCode,
+									   string[] permissions, Permission[] grantResults)
+		{
+			Console.WriteLine("got permission");
+			if (requestCode == CameraRequestPermission)
+			{
+				if (grantResults.Length > 0
+				    && grantResults[0] == Permission.Granted)
+				{
+					mDeniedCameraAccess = false;
+					if (!mPaused)
+					{
+						picker.StartScanning();
+					}
+				}
+				else {
+					mDeniedCameraAccess = true;
+				}
+				return;
+			}
+			base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+
 		public void OnCancel(IDialogInterface dialog) {
 			picker.StartScanning ();
 		}
 
 		protected override void OnResume () 
 		{
-			picker.StartScanning ();
+			Console.WriteLine("resume");
+			mPaused = false;
+			// handle permissions for Marshmallow and onwards...
+			if (Build.VERSION.SdkInt >= BuildVersionCodes.M)
+			{
+				grantCameraPermissionsThenStartScanning();
+			}
+			else {
+				// Once the activity is in the foreground again, restart scanning.
+				picker.StartScanning();
+			}
 			base.OnResume ();
 		}
 
@@ -119,7 +185,8 @@ namespace XamarinScanditSDKSampleAndroid
 			// collect objects without references asap but waits for a long time until finally collecting them.
 			GC.Collect ();
 			picker.StopScanning ();
-			base.OnPause ();
+			base.OnPause();
+			mPaused = true;
 		}
 
 		public override void OnBackPressed () 
