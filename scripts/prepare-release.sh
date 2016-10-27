83fbd21d38cce84e0f3f6b2a75d992d9db783086
Marco Biasini <marco@scandit.com>
2016-10-27 13:24:33 +0200

    update prepare release scripts

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -65,16 +65,16 @@ update_windows_csproj_files() {
 	xamarin_version_without_suffix=$(remove_version_suffix $XAMARIN_VERSION})
     all_csproj_files=$(find . -name '*.csproj')
     sed -i.bak -E \
-        "s/Scandit\.BarcodePicker\.[0-9]\.[0-9][0-9]*\.[0-9]*\.[0-9][0-9]*/Scandit.BarcodePicker.${xamarin_version_without_suffix}.0/g" \
+        "s/Scandit\.BarcodePicker\.[0-9]\.[0-9][0-9]*\.[0-9]*(\.[0-9][0-9]*)*/Scandit.BarcodePicker.${xamarin_version_without_suffix}/g" \
         $all_csproj_files
     sed -i.bak -E \
-        "s/Scandit\.Recognition\.[0-9]\.[0-9][0-9]*\.[0-9]*\.[0-9][0-9]*/Scandit.Recognition.${xamarin_version_without_suffix}.0/g" \
+        "s/Scandit\.Recognition\.[0-9]\.[0-9][0-9]*\.[0-9]*(\.[0-9][0-9]*)*/Scandit.Recognition.${xamarin_version_without_suffix}/g" \
         $all_csproj_files
     sed -i.bak -E \
         "s/(<Reference Include=\"Scandit[^,]*,.*Version=)[0-9](\.[0-9][0-9]*)*/\1${xamarin_version_without_suffix}.0/g" \
         $all_csproj_files
     sed -i.bak -E \
-        "s/(<Reference Include=\"VideoInputRT.Interop,.*Version=)[0-9](\.[0-9][0-9]*)*/\1${xamarin_version_without_suffix}.0/g" \
+        "s/(<Reference Include=\"VideoInputRT.Interop,.*Version=)[0-9](\.[0-9][0-9]*)*/\1${xamarin_version_without_suffix}/g" \
         $all_csproj_files
     sed -i.bak -E \
         "s/(<HintPath>.*Scandit\.BarcodePicker\.Xamarin\.)[0-9](\.[0-9][0-9]*)*/\1${xamarin_version_without_suffix}/g" \
@@ -83,7 +83,7 @@ update_windows_csproj_files() {
         "s/(<HintPath>.*Scandit\.BarcodePicker\.Unified\.)[0-9](\.[0-9][0-9]*)*/\1${xamarin_version_without_suffix}/g" \
         $all_csproj_files
     sed -i.bak -E \
-        "s/<ReleaseVersion>[0-9](\.[0-9][0-9]*)*<\/ReleaseVersion>/<ReleaseVersion>${xamarin_version_without_suffix}.0<\/ReleaseVersion>/g" \
+        "s/<ReleaseVersion>[0-9](\.[0-9][0-9]*)*<\/ReleaseVersion>/<ReleaseVersion>${xamarin_version_without_suffix}<\/ReleaseVersion>/g" \
         $all_csproj_files
     find . -name '*.csproj.bak' | xargs rm --
 }
